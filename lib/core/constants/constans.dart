9381bbdd10cccca98fa67a6ce01339a198be05b3
Muhammad Ahmed <ahmedkhan871871@gmail.com>
2023-04-02 17:15:15 -0700

    Updated
    Portfolio

---
+++ lib/core/constants/constans.dart
@@ -0,0 +1,12 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class Constants {
+
+  
+  double sWidth(c) => MediaQuery.of(c).size.width;
+  double sHeight(c) => MediaQuery.of(c).size.height;
+
+
+}
---
+++ lib/ui/main_screen/main_screen_vm.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainScreenVM with ChangeNotifier {
+
+final GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
+
+
+
+
+}
