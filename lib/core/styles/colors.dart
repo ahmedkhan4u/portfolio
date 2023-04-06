ca587569901c383e5312782acee1c814099718b8
Muhammad Ahmed <ahmedkhan871871@gmail.com>
2023-04-05 22:35:54 -0700

    Home Screen
    Work on home Screen
    5th April 2023

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const footerColor = Colors.black87;
+const headerTextColor = Colors.blue;
+const footerTextColor = Colors.white;
+const whiteColor = Colors.white;
+
