2619aadf1f80e69c7a01af1881b5bdaf679f108a
Muhammad Ahmed <ahmedkhan871871@gmail.com>
2023-05-08 21:32:42 -0700

    Updated
    8th April 2023

--- lib/ui/sections/body/contact_screen/contact_screen.dart
+++ lib/ui/sections/body/contact_screen/contact_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/constants/constans.dart';
 import 'package:portfolio/core/styles/text_styles.dart';
 
 class ContactScreen extends StatelessWidget {
@@ -6,6 +7,30 @@ class ContactScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Contact", style: headerTextBtnStyle,));
+    return Center(child: Container(
+      width: 500,
+      height: 300,
+      decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: const [
+                        BoxShadow(
+                            offset: Offset(2, 2), blurRadius: 1, spreadRadius: 1, color: Color.fromARGB(255, 206, 206, 206))
+                      ]),
+
+                      child: Column(
+                        children: [
+                          kHeight(16),
+                          Text("Contact Me",style: normalTextStyle.copyWith(fontSize: 22, color: Colors.blue),),
+                          Divider(
+                            height: 20,
+                            color: Color.fromARGB(255, 202, 202, 202),
+                             thickness: 1,
+),
+                        ],
+                      ),
+                      
+
+    ));
   }
 }
--- lib/ui/sections/body/resume_screen/resume_screen.dart
+++ lib/ui/sections/body/resume_screen/resume_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/styles/text_styles.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class ResumeScreen extends StatelessWidget {
   const ResumeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Resume", style: headerTextBtnStyle,));
+    return Container(
+          child: SfPdfViewer.asset('assets/my_cv.pdf'));
   }
 }
